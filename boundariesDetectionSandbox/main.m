f3a78126673520518f5f6f15c446c2ade8048c05
Andrew Paterson <asbuell@uh.edu>
2015-06-01 16:07:24 -0500

    Initial Commit

---
+++ boundariesDetectionSandbox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  boundariesDetectionSandbox
+//
+//  Created by Andrew Paterson on 6/1/15.
+//  Copyright (c) 2015 asb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
