53a03ec6f72af3d3aeaa8be93e833ec8e90e3558
Andrew Paterson <asbuell@uh.edu>
2015-06-04 18:05:28 -0500

    TODO: tweak UI bounds/space, incorporate next steps, perhaps invert colors of displayed image to better highlight?

--- boundariesDetectionSandbox/PixelCompositeImageGenerator.h
+++ boundariesDetectionSandbox/PixelCompositeImageGenerator.h
@@ -2,7 +2,7 @@
 //  PixelCompositeImageGenerator.h
 //  boundariesDetectionSandbox
 //
-//  Created by Andrew Paterson on 6/2/15.
+//  Created by Alex on 6/2/15.
 //  Copyright (c) 2015 asb. All rights reserved.
 //
 
